6d47ea6dbbe9ba5c072dd41a6ebf18e6abe23830
Jesse Marino <jessemarino2020@gmail.com>
2022-04-05 23:15:19 -0700

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # do-you-even-code
+
+My motivation to build "do you even code" was to create a test for new coders. I wanted to test my skills at creating a quiz/test game that would challange me and other new developers. "Do you even code" solves the problem of you wondering if you can code or not, it solved mine. I learned that I need to futher research /learn Javascript, but I also learned how to use inner.html. Please note this project isn't finished.
+
+![website screenshot](assets/images/shot1.png)
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,4 @@
-var timerEl = document.querySelector("timebox");
+var timeEl = document.querySelector("timebox");
 var startBtn = document.querySelector(".startbutt");
 var questbox = document.querySelector(".quest");
 var intro = document.querySelector(".intro");
@@ -9,35 +9,46 @@ var q3 = "What is the tag for paragraph?";
 var q4 = "What is the tag for creating a organized list?";
 var q5 = "what is the tag for creating a list item?";
 
-var a1 = "cascading style sheet";
+var a1 = ["cascading style sheet", "yes", "no", "no"];
 var a2 = "hypertext markup language";
 var a3 = "<p>";
 var a4 = "<ol>";
-var a5 = "<li>";
+//var a5 = "<li>";
 
 var questions = [q1, q2, q3, q4, q5 ];
-var answers = [a1, a2, a3, a4, a5];
-var timerCount;
+var answers = [a1, a2, a3, a4];
+var secondsLeft = 10;
 var timer;
 
 function startGame() {
-    timerEl = "";
+    timeEl = "";
     startBtn.disabled = true;
-    //var introduction = document.getElementById(".intro")
-    //introduction.style.display = "none";
+    //var introduction = document.getElementById("#intro").style.display = "none";
     startTimer()
     quizQA()
   }
 
   function startTimer() {
-   var timer = setInterval(function() {
-      timerCount--;
-      timer.textContent = timerEl;
+   var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft;
+
+      if(secondsLeft === 0) {
+          clearInterval(timerInterval);
+
+          //youlost()
+      }
     }, 1000);
   }
 
 function quizQA() {
-
+    for (var i = 0; i < questions.length; i++) {
+        document.getElementById("quest").innerHTML = q1;
+    }
+    for (var j = 0; j < answers.length; j++) {
+        document.getElementById("ansbutton1").innerHTML = a1;
+    }
+    //if (//if correct answer is picked add time and else incorrect minus time )
 }
 
 
